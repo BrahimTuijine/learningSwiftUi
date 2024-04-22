63b329e8763ce18b3a5d082d7773824c3074985d
Brahim tuijine <brahimtuijine220@gmail.com>
2024-04-22 09:47:56 -0700

    Initial Commit

---
+++ learningSwiftUi/learningSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningSwiftUiApp.swift
+//  learningSwiftUi
+//
+//  Created by MacBook on 22/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct learningSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
